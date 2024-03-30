8697368fa80edf5aab9d18ea0c6a851a83316e72
gyeom <rlagusrua3687@gmail.com>
2024-03-30 16:34:36 +0900

    PLUG-104 feat: S3 설정 파일, 서비스 클래스 구현

---
+++ src/main/java/com/justdo/plug/blog/global/s3/S3Result.java
@@ -0,0 +1,11 @@
+package com.justdo.plug.blog.global.s3;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class S3Result {
+
+    private String imgUrl;
+}
