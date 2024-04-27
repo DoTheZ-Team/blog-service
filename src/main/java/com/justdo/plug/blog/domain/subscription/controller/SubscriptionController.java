f33cfd7e1c49febb76503af16155934c71fcbfa8
gyeom <rlagusrua3687@gmail.com>
2024-04-27 16:51:06 +0900

    GLUE-120 feat: Blog-Service에 Swagger 적용

--- src/main/java/com/justdo/plug/blog/domain/subscription/controller/SubscriptionController.java
+++ src/main/java/com/justdo/plug/blog/domain/subscription/controller/SubscriptionController.java
@@ -4,6 +4,10 @@ import com.justdo.plug.blog.domain.subscription.dto.SubscriptionResponse.Subscri
 import com.justdo.plug.blog.domain.subscription.service.SubscriptionCommandService;
 import com.justdo.plug.blog.global.response.ApiResponse;
 import com.justdo.plug.blog.global.utils.JwtProvider;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Subscription API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/blogs/subscription")
@@ -19,6 +24,8 @@ public class SubscriptionController {
     private final JwtProvider jwtProvider;
     private final SubscriptionCommandService subscriptionCommandService;
 
+    @Operation(summary = "블로그 구독 요청 및 취소 요청", description = "다른 블로그를 구독 요청합니다. / 구독 상태에서 누르면 취소됩니다.")
+    @Parameter(name = "blogId", description = "블로그 Id, Path Variable입니다.", required = true, example = "1", in = ParameterIn.PATH)
     @PostMapping("/{blogId}")
     public ApiResponse<SubscriptionProc> subscribe(HttpServletRequest request,
         @PathVariable(name = "blogId") Long blogId) {
